ef352827daf2762d27c346596b50d8f6ec70942c
DESKTOP-5QM2G6V\ranad <ranadheerleo@gmail.com>
2017-09-13 21:54:29 -0500

    Intial Commit

---
+++ src/control/statements/OddEven.java
@@ -0,0 +1,5 @@
+package control.statements;
+
+public class OddEven {
+
+}
---
+++ src/control/statements/OddEvenTester.java
@@ -0,0 +1,10 @@
+package control.statements;
+
+public class OddEvenTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
